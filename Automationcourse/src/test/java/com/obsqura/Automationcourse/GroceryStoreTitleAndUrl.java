08a26b992197c2d447a7b94e478d8f2de4da6eae
VyshnavyPrakash <vyshnavyprakash18@gmail.com>
2022-11-10 19:24:35 +0530

    Automation course

--- Automationcourse/src/test/java/com/obsqura/Automationcourse/GroceryStoreTitleAndUrl.java
+++ Automationcourse/src/test/java/com/obsqura/Automationcourse/GroceryStoreTitleAndUrl.java
@@ -1,13 +1,15 @@
 package com.obsqura.Automationcourse;
 
+import org.junit.Assert;
 
 public class GroceryStoreTitleAndUrl extends GroceryStore {
 	
 
 	public void getTitle() {
 		
-		
+		String expectedTitle = "Grocerystore";
 		String title = driver.getTitle();
+		Assert.assertEquals(expectedTitle, title);
 	}
 	
 	public void getURL() {
